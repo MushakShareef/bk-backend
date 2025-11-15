4f4caed94486435f2b545f36fff42e358b0ae2c1
Mushak Shareef <sivcanopi@gmail.com>
2025-11-15 15:27:43 +0530

    2

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors({
     origin: [
         'http://localhost:3000',
         'http://localhost:5173',
-        'https://try-bk-chart.vercel.app/',
+        'https://try-bk-chart.vercel.app',
         process.env.FRONTEND_URL
     ].filter(Boolean),
     credentials: true,
@@ -22,6 +22,16 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://try-bk-chart.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+
 
 // Handle preflight requests
 app.options('*', cors());
